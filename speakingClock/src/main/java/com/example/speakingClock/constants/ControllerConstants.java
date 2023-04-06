1a4a7ad36f31b8458bcc194a1ff744835a9c00b6
purush202304 <entertainmentreddy2021@gmail.com>
2023-04-06 11:53:26 +0800

    removed unwanted files

--- speakingClock/src/main/java/com/example/speakingClock/constants/ControllerConstants.java
+++ speakingClock/src/main/java/com/example/speakingClock/constants/ControllerConstants.java
@@ -3,6 +3,5 @@ package com.example.speakingClock.constants;
 public class ControllerConstants {
 
     private ControllerConstants(){}
-
     public static final String SPEAKING_CLOCK="/speaking-clock";
 }
